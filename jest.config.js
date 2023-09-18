b61d934973f18aeda40b2706959c2cd89c2c5a62
Felipe <felipemiillerr@gmail.com>
2023-09-18 09:30:07 -0300

    fix: refactoring  configurations of test

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testiIgnorePatterns: ["/node_modules/", "/.next/"],
-    setupFilesAfterEnv: ["<rootDir>/src/config/jest/jest.setup.js"],
-    transform: {
-        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-    },
-    testEnvironment: "jsdom",
-}
