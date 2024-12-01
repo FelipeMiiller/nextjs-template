32ade8bf9210e195f49422d7b3c79eaddce4d558
Felipe Miiller <felipemiillerr@gmail.com>
2024-12-01 10:26:24 -0300

    add services

---
+++ src/services/fetch/index.ts
@@ -0,0 +1,29 @@
+import { ZodTypeAny } from "zod"
+
+export async function fetchWrapper<T>({
+  input,
+  init,
+  schemaValidator,
+}: {
+  input: URL | RequestInfo
+  init?: RequestInit | undefined
+  schemaValidator?: ZodTypeAny
+}): Promise<T> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const data = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/${input}`,
+        init
+      )
+      const result: T = await data.json()
+      if (schemaValidator) {
+        const zod = schemaValidator.parse(result)
+        resolve(zod as T)
+      }
+
+      resolve(result)
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
